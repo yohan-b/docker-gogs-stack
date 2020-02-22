ff6836058c71cd57b2d59d393b058cab671417df
yohan <783b8c87@scimetis.net>
2020-02-22 21:44:10 +0100

    Added mounting volume with openstack.

--- create.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker volume create --name gogs_data
-chcon -R -u system_u -r object_r -t svirt_sandbox_file_t /var/lib/docker-latest/volumes/gogs_data/_data/
-docker-compose up -d
---
+++ start_or_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+test -f ~/openrc.sh || { echo "ERROR: ~/openrc.sh not found, exiting."; exit 1; }
+source ~/openrc.sh
+INSTANCE=$(~/env_py3/bin/openstack server show -c id --format value $(hostname))
+for VOLUME in gogs_data
+do
+    if ! mountpoint -q /mnt/volumes/${VOLUME}
+    then
+         VOLUME_ID=$(/home/yohan/env_py3/bin/openstack volume show ${VOLUME} -c id --format value)
+         test -e /dev/disk/by-id/*${VOLUME_ID:0:20} || nova volume-attach $INSTANCE $VOLUME_ID auto
+         sleep 3
+         sudo mount /dev/disk/by-id/*${VOLUME_ID:0:20} /mnt/volumes/${VOLUME}
+         mountpoint -q /mnt/volumes/${VOLUME} || { echo "ERROR: could not mount /mnt/volumes/${VOLUME}, exiting."; exit 1; }
+    fi
+done
+
+sudo -E bash -c 'docker-compose up -d'
